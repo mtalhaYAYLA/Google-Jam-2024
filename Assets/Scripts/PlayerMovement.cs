1b4dea9faa1cebcaa0b57512436d28086282b79f
Mert <mertarslanc@gmail.com>
2024-05-19 23:41:21 +0300

    Kapı eklendi

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,6 +11,8 @@ public class PlayerMovement : MonoBehaviour
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
     public float lookXLimit = 45.0f;
+    public GameObject ilkKagit;
+    public bool ilkKagitAcik;
 
     CharacterController characterController;
     Vector3 moveDirection = Vector3.zero;
@@ -22,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
-
+        ilkKagitAcik = true;
         // Lock cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -67,6 +69,12 @@ public class PlayerMovement : MonoBehaviour
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H) && ilkKagitAcik)
+        {
+            ilkKagitAcik = false;
+            ilkKagit.SetActive(false);
         }
     }
 }
