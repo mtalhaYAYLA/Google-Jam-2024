78eff4a76722686877ca2cee4b68b8e6183a1dbc
Mert <mertarslanc@gmail.com>
2024-05-20 19:04:46 +0300

    Ohoo çok şey

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void LoadSceneOne()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
--- Assets/Scripts/PlayerTrigger.cs
+++ Assets/Scripts/PlayerTrigger.cs
@@ -7,6 +7,8 @@ public class PlayerTrigger : MonoBehaviour
     public GameObject monster;
     public Transform monsterStartPoint;
     public GameObject triggerPlayer;
+    public GameObject arkaKapi;
+    public Animator anim;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,7 +17,10 @@ public class PlayerTrigger : MonoBehaviour
             //Burada Canavar Serbest Kaldý UI Açýlacak ve belli bir süre sonra kapatýlacak
             Debug.Log("Player trigger entered. Resetting monster position.");
             monster.transform.position = monsterStartPoint.position;
+            anim = arkaKapi.GetComponent<Animator>();
+            anim.Play("Close");
             monster.SetActive(true); // Canavarý aktif hale getiriyoruz
+
             Destroy(triggerPlayer);
 
         }
