ff542b7b097c047537d11a60cd6a3dc8ac27c55c
Yasin Çağatay Özbek <yasincagatayozbek@gmail.com>
2024-05-20 10:17:37 +0300

    UI Düzenlemeleri Yapıldı

---
+++ Assets/Scripts/Gosterge.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gosterge : MonoBehaviour
+{
+    public TextMeshProUGUI sayac;
+    void Update()
+    {
+        sayac.text = LanternFuel.getFuelAmount().ToString("F2");
+    }
+}
