26c6b947464e73e563842ab9578498e3b085f0ac
Mert <mertarslanc@gmail.com>
2024-05-20 22:13:53 +0300

    Oyun Bitti

--- Assets/FinishGame.cs
+++ Assets/FinishGame.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject playerCharacter;
+    public GameObject triggerPlayer;
+    
+
+    public void LoadScene(int sceneNumber)
     {
-        
+        SceneManager.LoadScene(sceneNumber);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            LoadScene(4);
+        }
     }
 }
